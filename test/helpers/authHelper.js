92216aa05288da3eed25ff504f8a72b8cbeb88a1
Simone <smgabionetta@yahoo.com.br>
2025-11-07 15:22:45 +0000

    feito ajusto no arquivo authHelpers, criado usuario de testes e login antes de cada teste

--- test/helpers/authHelper.js
+++ test/helpers/authHelper.js
@@ -3,15 +3,26 @@ const request = require('supertest');
 const logins = require('../fixtures/requisicoes/users/postUsersLogin.json');
 
 const obterToken = async () => {
+    // Registrar usuário antes do login (ignora se já existir)
+    await request(process.env.BASE_URL)
+        .post('/users/register')
+        .send({
+            name: 'Usuário Teste',
+            email: logins.validLogin.email,
+            password: logins.validLogin.password
+        });
+
+    // Fazer login para obter o token
     const respostaLogin = await request(process.env.BASE_URL)
         .post('/users/login')
-        .send({   
-        email: logins.validLogin.email,
-        password: logins.validLogin.password
-        })
-    return respostaLogin.body.token
- 
+        .send({
+            email: logins.validLogin.email,
+            password: logins.validLogin.password
+        });
+
+    return respostaLogin.body.token;
 };
+
 
 module.exports ={
     obterToken
